49ac930cf74ee7549c8e41f0ce3d49b56d09d0fc
Giga Maisuradze <98545556+GigaMaisa@users.noreply.github.com>
2023-03-02 23:51:51 +0400

    Update DataProviderTest.java

--- src/test/java/DataProviderTest.java
+++ src/test/java/DataProviderTest.java
@@ -3,17 +3,14 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class DataProviderTest {
-
-
         @DataProvider
         public Object[][] dpMethod(){
-            return new Object[][] {{2, 3 , 5}, {5, 8, 11}};
+            return new Object[][] {{2, 3 , 5}, {5, 8, 13}};
         }
 
         @Test(dataProvider = "dpMethod")
         public void myTest (int a, int b, int result) {
             int sum = a + b;
             Assert.assertEquals(result, sum) ;
-
     }
 }
